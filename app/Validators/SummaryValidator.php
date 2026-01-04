dc7e64c1627d50e30865b48e9341ccc59f33ff6a
Ethan <cherub0526@gmail.com>
2026-01-05 03:04:15 +0800

    fix: 強制並報告摘要欄位的驗證錯誤
    - 為 short_summary 和 long_summary 欄位新增驗證錯誤訊息，包括嵌套陣列內容如 content、key_points 和 keywords
    - 定義 short_summary 和 long_summary 欄位的驗證規則，強制要求資料類型及嵌套屬性的存在
    
    Signed-off-by: Ethan <cherub0526@gmail.com>

--- app/Validators/SummaryValidator.php
+++ app/Validators/SummaryValidator.php
@@ -11,18 +11,33 @@ class SummaryValidator extends BaseValidator
         parent::__construct($params);
 
         $this->messages = [
-            'locale.required' => __('validators.summary.locale.required'),
-            'locale.string'   => __('validators.summary.locale.string'),
-            'text.required'   => __('validators.summary.text.required'),
-            'text.array'      => __('validators.summary.text.array'),
+            'locale.required'                       => __('validators.summary.locale.required'),
+            'locale.string'                         => __('validators.summary.locale.string'),
+            'text.required'                         => __('validators.summary.text.required'),
+            'text.array'                            => __('validators.summary.text.array'),
+            'text.short_summary.required'           => __('validators.summary.text.short_summary.required'),
+            'text.short_summary.string'             => __('validators.summary.text.short_summary.string'),
+            'text.long_summary.required'            => __('validators.summary.text.long_summary.required'),
+            'text.long_summary.array'               => __('validators.summary.text.long_summary.array'),
+            'text.long_summary.content.required'    => __('validators.summary.text.long_summary.content.required'),
+            'text.long_summary.content.string'      => __('validators.summary.text.long_summary.content.string'),
+            'text.long_summary.key_points.required' => __('validators.summary.text.long_summary.key_points.required'),
+            'text.long_summary.key_points.array'    => __('validators.summary.text.long_summary.key_points.array'),
+            'text.long_summary.keywords.required'   => __('validators.summary.text.long_summary.keywords.required'),
+            'text.long_summary.keywords.array'      => __('validators.summary.text.long_summary.keywords.array'),
         ];
     }
 
     public function setStoreRules(): self
     {
         $this->rules = [
-            'locale' => 'required|string',
-            'text'   => 'required|array',
+            'locale'                       => 'required|string',
+            'text'                         => 'required|array',
+            'text.short_summary'           => 'required|string',
+            'text.long_summary'            => 'required|array',
+            'text.long_summary.content'    => 'required|string',
+            'text.long_summary.key_points' => 'required|array',
+            'text.long_summary.keywords'   => 'required|array',
         ];
 
         return $this;
